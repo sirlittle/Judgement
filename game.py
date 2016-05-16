261b83c18cddbae9179933bc3e3fb91448dc8b73
Salil Vanvari <salilvanvari@berkeley.edu>
2016-05-15 21:06:40 -0700

    Created a deck object and card object

--- game.py
+++ game.py
@@ -1,7 +1,10 @@
-class Card:
-    suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
-    rank = [str(i) for i in range(2,11)] + ["Jack", "Queen", "King", "Ace"]
-    def _init_(self, suit, rank):
+from Deck import *
+from Card import *
+
 
 playGame = True
+deck = Deck()
+print(deck.deal())
 while playGame:
+    playGame = False
+print("done with game")
