33ae397644036deb2e8cd7725e75e8957e7b5ebf
Salil Vanvari <salilvanvari@berkeley.edu>
2016-08-05 01:05:27 -0700

    Added a stats collection Object along with a run of data collected through 100k games of Random Bot.

--- game.py
+++ game.py
@@ -1,15 +1,18 @@
 from util.card import *
 from util.deck import *
 from util.player import *
+from util.stats import *
+import csv
 
 # Returns the score of a given hand.
 def getScore(num):
     return (num+1)*10 + num
 
 # Simulates a single round of Judgement
-def round(players, numCards, deck, scores):
+def round(players, numCards, deck, scores, stats):
     predictions = {}
     sumPredicts = 0
+    # Get all Player predictions.
     for i in range(len(players)):
         player = players[i]
         player.hand = deck.deal(numCards)
@@ -24,11 +27,12 @@ def round(players, numCards, deck, scores):
         for player in players:
             played = player.choose(cardsPlayed, deck.trump)
             cardsPlayed += [played]
-        winner,winningCard = deck.chooseWinner(cardsPlayed,players)
+        winner, winningCard = deck.chooseWinner(cardsPlayed,players)
         winnerName = players[winner].name
         players[winner].win()
         players = players[winner:] + players[0:winner]
         curHandsMade[winnerName] += 1
+        stats.collectRoundData(winningCard, winner, deck.trumpSuit, numCards)
         # print("Winner of this hand: " + winnerName)
         # print("Winning Card: " + str(winningCard))
         # print("Cards in hand: " + str(cardsPlayed))
@@ -41,17 +45,18 @@ def round(players, numCards, deck, scores):
         player.reset()
 
 # Runs a game loop
-def playGame(players):
+def playGame(players, numTimes=100000):
     playGame = True
     scores = {player.name:0 for player in players}
-    for _ in range(1000):
+    stats = JudgementStats()
+    for _ in range(numTimes):
         for i in range(7):
             deck = JudgementDeck()
             numCards = 7-i
             # print("starting round " + str(numCards))
             # print("\n\n")
 
-            round(players, numCards, deck, scores)
+            round(players, numCards, deck, scores, stats)
             # print("after round scores are: " + str(scores))
             # print("\n\n")
         for i in range(1,8):
@@ -59,13 +64,14 @@ def playGame(players):
             numCards = i
             # print("starting round " + str(numCards))
             # print("\n\n")
-            round(players, numCards, deck, scores)
+            round(players, numCards, deck, scores, stats)
             # print("after round scores are: " + str(scores))
             # print("\n\n")
         # print("Final scores are " + str(scores))
         playGame = False
+    stats.writeToCSV("dataRun1.csv")
     print("Final scores are " + str(scores))
 
-playGame([AggressiveBot("Bob" + str(i)) for i in range(5)] + [AggressiveBot("IWIN")])
+playGame([Player("Bob" + str(i)) for i in range(5)])
 
 
--- util/card.py
+++ util/card.py
@@ -11,4 +11,4 @@ class Card:
         return self.name
 
     def __repr__(self):
-        return self.name
+        return self.name        
